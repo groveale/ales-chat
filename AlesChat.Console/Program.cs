ff60ddd8e61d22066f689d12355bead466502ea9
groveale <groveale@users.noreply.github.com>
2019-01-23 13:16:18 +0000

    simplified singnalr code

--- AlesChat.Console/Program.cs
+++ AlesChat.Console/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.Threading.Tasks;
+using AlesChat.Framework;
 
-namespace AlesChat.Console
+namespace AlesChat.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static ChatService service;
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            service = new ChatService();
+            service.OnReceivedMessage += Service_OnReceivedMessage;
+            service.OnTypingMessage += Service_OnTypingMessage;
+            service.OnEnteredOrExitMessage += Service_OnEnteredOrExitMessage;
+
+            Console.WriteLine("Enter IP:");
+            var ip = Console.ReadLine();
+            service.Init(ip);
+
+            try
+            {
+                await service.ConnectAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Connection Error: {ex.Message}");
+            }
+           
+
+            Console.WriteLine("You are connected...");
+            Console.WriteLine("Enter room(.Net, Xamarin, Azure, PowerShell):");
+
+            var room = Console.ReadLine();
+
+            await service.JoinChannelAsync(room, "console app");
+
+            var keepRunning = true;
+            do
+            {
+                var text = Console.ReadLine();
+                if (text == "exit")
+                {
+                    keepRunning = false;
+                }
+                else
+                {
+                    await service.SendMessageAsync(room, "console app", text);
+                }
+            }
+            while (keepRunning);
         }
+
+        static void Service_OnReceivedMessage(object sender, Framework.EventHandlers.MessageEventsArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        static void Service_OnTypingMessage(object sender, Framework.EventHandlers.MessageEventsArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        static void Service_OnEnteredOrExitMessage(object sender, Framework.EventHandlers.MessageEventsArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
     }
 }
--- AlesChat.Framework/ChatService.cs
+++ AlesChat.Framework/ChatService.cs
@@ -1,10 +1,110 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AlesChat.Framework.EventHandlers;
+using Microsoft.AspNetCore.SignalR.Client;
+
 namespace AlesChat.Framework
 {
     public class ChatService
     {
-        public ChatService()
+        public event EventHandler<MessageEventsArgs> OnReceivedMessage;
+        public event EventHandler<MessageEventsArgs> OnTypingMessage;
+        public event EventHandler<MessageEventsArgs> OnEnteredOrExitMessage;
+        public event EventHandler<MessageEventsArgs> OnConnectionClosed;
+        //public event EventHandler<MessageEventsArgs> OnConnected;
+
+        HubConnection hubConnection;
+        Random random;
+
+        bool IsConnected { get; set; }
+        Dictionary<string, string> ActiveChannels { get; } = new Dictionary<string, string>();
+
+        public void Init(string ip)
         {
+            hubConnection = new HubConnectionBuilder()
+                .WithUrl($"http://{ip}:5000/hubs/chat")
+                .Build();
+
+            hubConnection.Closed += async (error) =>
+            {
+                OnConnectionClosed?.Invoke(this, new MessageEventsArgs("Connection closed..."));
+                IsConnected = false;
+                await Task.Delay(random.Next(0, 5) * 1000);
+                try
+                {
+                    await ConnectAsync();
+                }
+                catch (Exception ex)
+                {
+                    // catch
+                }
+            };
+
+            hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                var finalMessage = $"{user} says {message}";
+                OnReceivedMessage?.Invoke(this, new MessageEventsArgs(finalMessage));
+                OnTypingMessage?.Invoke(this, new MessageEventsArgs(string.Empty));
+            });
+
+            hubConnection.On<string>("EnteredOrLeft", (message) =>
+            {
+                OnEnteredOrExitMessage?.Invoke(this, new MessageEventsArgs(message));
+            });
+
+            hubConnection.On<string>("TypingMessage", (user) =>
+            {
+                OnTypingMessage?.Invoke(this, new MessageEventsArgs($"{user} is typing..."));
+            });
+        }
+
+        public async Task ConnectAsync()
+        {
+            if (IsConnected)
+                return;
+                         
+            await hubConnection.StartAsync();
+            IsConnected = true;
+            
+        }
+
+        public async Task DisconnectAsync()
+        {
+            if (!IsConnected)
+                return;
+                
+
+            ActiveChannels.Clear();
+
+            await hubConnection.StopAsync();
+            IsConnected = false;
+        }
+
+        public async Task JoinChannelAsync(string group, string userName)
+        {
+            if (!IsConnected || ActiveChannels.ContainsKey(group))
+                return;
+                
+            await hubConnection.SendAsync("AddToGroup", group, userName);
+            ActiveChannels.Add(group, userName);
+        }
+
+        public async Task LeaveChannelAsync(string group, string userName)
+        {
+            if (!IsConnected || !ActiveChannels.ContainsKey(group))
+                return;
+
+            await hubConnection.SendAsync("RemoveFromGroup", group, userName);
+            ActiveChannels.Remove(group);
+        }
+
+        public async Task SendMessageAsync (string group, string userName, string message)
+        {
+            await hubConnection.InvokeAsync("SendMessageGroup",
+                    group,
+                    userName,
+                    message);
         }
     }
 }
